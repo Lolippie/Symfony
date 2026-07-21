36d7120e52ce030aa0dc0ae048be28d9b124985d
Lolippie <charlot.noemie72@gmail.com>
2026-07-21 17:15:24 +0200

    feat: login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher){}
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@admin.fr');
+        $admin->setPassword($this->hasher->hashPassword($admin, '1234'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
